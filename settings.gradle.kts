af660abec9d6cc5a16f149af382938b82425cebf
Swantan Barua <work.swantan@gmail.com>
2025-09-08 14:30:31 +0530

    Import modules in settings gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,3 +35,18 @@ plugins {
 include(":composeApp")
 include(":core-database")
 include(":core-network")
+
+// SEARCH
+include(":search-ui")
+include(":search-data")
+include(":search-domain")
+
+// FAVOURITE
+include(":favourite-ui")
+include(":favourite-data")
+include(":favourite-domain")
+
+// GAME
+include(":game-ui")
+include(":game-data")
+include(":game-domain")
