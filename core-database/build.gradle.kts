fc864b3f4a2025628f563627433ab64965cb1f43
Swantan Barua <work.swantan@gmail.com>
2025-09-08 13:45:39 +0530

    Include core-database module in settings gradle

--- core-database/build.gradle.kts
+++ core-database/build.gradle.kts
@@ -60,8 +60,6 @@ android {
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
     }
     packaging {
         resources {
